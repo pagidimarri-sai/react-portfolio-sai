ccf9d85047005dbd8eeef2d6a33491bf59118b50
pagidimarri sai <pagidimarrisai27@gmail.com>
2024-09-23 23:37:36 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import aboutImg from "../assets/about.jpg";
+import { motion } from "framer-motion";
+
+const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+const About = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-4">
+      <h1 className="my-20 text-center text-4xl">About 
+        <span className="text-neutral-500"> Me</span>
+      </h1>
+      <div className="flex flex-wrap">
+        <motion.div
+        whileInView={{ opacity: 1, x: 0}}
+        initial={{ opacity:0, x: -100}}
+        transition={{ duration:0.5}}
+        className="w-full lg:w-1/2 lg:p-8">
+          <div className="flex items-center justify-center">
+            <img className="rounded-2xl" src={aboutImg} alt="about" />
+          </div>
+        </motion.div>
+        <motion.div 
+        whileInView={{ opacity:1, x: 0}}
+        initial={{ opacity:0, x:100}}
+        transition={{duration:0.5}}
+
+        className="w-full lg:w-1/2">
+          <div className="flex justify-center lg:justify-start">
+            <p className="my-2 max-w-xl py-6">{ABOUT_TEXT}</p>
+          </div>
+        </motion.div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/constants/index.js
@@ -0,0 +1,76 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications.I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2023 - Present",
+    role: "Full Stack Developer",
+    company: "Personal Project - Gym Website",
+    description: `Developed a fully responsive website for managing gym membership and schedules using HTML, CSS, JavaScript, and ReactJS. Integrated back-end functionalities with Node.js and MongoDB.`,
+    technologies: ["HTML", "CSS", "JavaScript", "ReactJS", "Node.js", "MongoDB"],
+  },
+  {
+    year: "2022 - 2023",
+    role: "Web Development Intern",
+    company: "Internship",
+    description: `Developed and optimized user interfaces using HTML, CSS, and JavaScript. Collaborated with team members on feature implementations and bug fixes.`,
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+  {
+    year: "2021 - 2022",
+  role: "Open Source Contributor",
+  company: "Open Source Project - GitHub",
+  description: `Worked on enhancing the UI and fixing bugs for a JavaScript-based open-source project on GitHub. Improved overall user experience and solved multiple design-related issues.`,
+  technologies: ["JavaScript", "GitHub", "CSS"],
+  },
+  {
+    year: "2021 - 2022",
+    role: "Open Source Contributor",
+    company: "Open Source Project - GitHub",
+    description: `Worked on enhancing the UI and fixing bugs for a JavaScript-based open-source project on GitHub. Improved overall user experience and solved multiple design-related issues.`,
+    technologies: ["JavaScript", "GitHub", "CSS"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Task Management App",
+    image: project2,
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Blogging Platform",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  },
+];
+
+export const CONTACT = {
+  address: "Hyderabad, Nalgonda, 509254 ",
+  phoneNo: "+919398083729 ",
+  email: "pagidimarrisai27@gmail.com",
+};
