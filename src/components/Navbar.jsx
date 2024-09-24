99f0c0206d283f7592bb93e1675492811db98ab8
pagidimarri sai <pagidimarrisai27@gmail.com>
2024-09-24 17:03:59 +0530

    Updated Navbar with color and rotation effects

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import logo from "../assets/kevinRushProfile1.jpg";
-import { FaLinkedin} from "react-icons/fa";
-import { FaGithub} from "react-icons/fa";
+import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
-import { FaInstagram} from "react-icons/fa";
 
 const Navbar = () => {
   return (
@@ -12,13 +10,41 @@ const Navbar = () => {
         <img className="mx-2 w-10" src={logo} alt="logo" />
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl"> 
-        <FaLinkedin />
-        <FaGithub />
-        <FaInstagram />
-        <FaSquareXTwitter />
+        <a 
+          href="https://www.linkedin.com/in/sai-chary-810828287/" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="hover:text-[#0077B5] active:rotate-180 transition-transform duration-300"
+        >
+          <FaLinkedin />
+        </a>
+        <a 
+          href="https://github.com/pagidimarri-sai" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="hover:text-white active:rotate-180 transition-transform duration-300"
+        >
+          <FaGithub />
+        </a>
+        <a 
+          href="https://www.instagram.com/yourProfile" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="hover:text-[#E1306C] active:rotate-180 transition-transform duration-300"
+        >
+          <FaInstagram />
+        </a>
+        <a 
+          href="https://x.com/home" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="hover:text-[#1DA1F2] active:rotate-180 transition-transform duration-300"
+        >
+          <FaSquareXTwitter />
+        </a>
       </div>
     </nav>
   );
 }
 
-export default Navbar; // Default export
+export default Navbar;
